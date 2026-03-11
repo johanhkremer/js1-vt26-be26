9b21bd14df739ad194ecb63e8b37749d589dd41e
Johan Kremer <johan.h.kremer@gmail.com>
2026-03-11 13:45:54 +0100

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,25 @@
+console.log("Hello from script.js ⚡️")
+
+//Let Får ändra data
+let teacher = "Johan"
+let age = 44
+let courseStarted = true
+
+//Const Får inte ändra data
+const courseLengthWeeks = 20
+
+console.log("Lärare i kursen", teacher)
+
+teacher = "stefan"
+
+console.log("Lärare i kursen", teacher)
+
+console.log("teacter datatyp ?" + typeof teacher)
+console.log("age datatyp ?" + typeof age)
+console.log("course started datatyp ?" + typeof courseStarted)
+
+const user = "johan"
+
+console.log("user:", teacher, "datatyp:", typeof teacher)
+
+//Datatyper: number, string, boolean
