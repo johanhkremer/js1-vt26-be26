508f30c6b5de941a267c2f37c2da7b6c884c9cb2
Johan Kremer <johan.h.kremer@gmail.com>
2026-03-18 10:11:21 +0100

    clean code from last lesson, add example for truthy and falsy

--- script/script.js
+++ script/script.js
@@ -1,65 +1,41 @@
-// console.log(1)
-// console.log(2)
-// console.log(3)
-// console.log(4)
-// console.log(5)
-// console.log(6)
-// console.log(7)
-// console.log(8)
-// console.log(9)
-// console.log(10)
+/*
+----------------------------------------------------------------
+? Falsy
+----------------------------------------------------------------
+Value	        Type	    Description
+null	        Null	    The keyword null — the absence of any value.
+undefined	    Undefined	undefined — the primitive value.
+false	        Boolean	    The keyword false.
+NaN	            Number	    Not a Number (represents an invalid or undefined result from a calculation.)
+0	            Number	    The Number zero, also including 0.0, 0x0, etc.
+-0	            Number	    The Number negative zero, also including -0.0, -0x0, etc.
+0n	            BigInt	    The BigInt zero, also including 0x0n, etc. Note that there is no BigInt negative zero — the negation of 0n is 0n.
+""	            String	    Empty string value, also including '' and ``.
+document.all	Object	    The only falsy object in JavaScript is the built-in document.all.
 
-//Inte DRY, väldigt upprepande
+----------------------------------------------------------------
+? Truthy
+----------------------------------------------------------------
+All values are truthy except false values.
+*/
 
-//!for loop (Används oftast när du redan vet ungefär hur många gånger något ska köras.)
+const matte = 1 + "7"
 
-//! (loop variabel, villkor, uppdatering) {Gör någonting}
-
-for (let i = 1; i <= 10; i++) {
-    console.log("🔁 for loop", i)
+if (matte) {
+    console.log(matte)
+    console.log(typeof matte)
+    console.log("Detta värdet är truthy ✅")
+} else {
+    console.log(matte)
+    console.log(typeof matte)
+    console.log("Detta värdet är falsy ❌")
 }
 
-//!while loop (Används när du vill köra så länge ett villkor är sant, men du inte nödvändigtvis vet i förväg hur många varv det blir.)
+/*  && → första falsy, annars sista */
 
-let i = 0
+/*  || → första truthy, annars sista */
 
-while (i <= 10) {
-    if (i <= 0) {
-        console.log("Starta räkningen!")
-    }
+/*  ?? tar nästa värde bara om vänster sida är null eller undefined */
 
-    console.log("🔁 while loop", i)
+/*  ! → vänder till motsatt boolean */
 
-    if (i >= 10) {
-        console.log("Räkning färdig!")
-    }
-
-    i++
-}
-
-//!do-while loop (Fungerar nästan som while, men den kör alltid minst en gång eftersom villkoret kollas efteråt.)
-
-let number = 0
-
-do {
-    console.log("🔁 do-while loop", number)
-    number++
-}
-
-while (number <= 10)
-
-//! Operator %
-//Operatorn % ger resten efter en division.
-
-console.log("10 % 2 = ", 10 % 2)
-console.log("9 % 2 = ", 9 % 2)
-
-for (let i = 0; i <= 20; i++) {
-    if (i === 0) {
-        console.log("Endast jämna tal")
-    }
-
-    if (i % 2 === 0) {
-        console.log(i)
-    }
-}
