fe8b4d8682279b04ae9ffc9d8481d25d3f6176da
Johan Kremer <johan.h.kremer@gmail.com>
2026-04-01 11:58:47 +0200

    refactor app with array with objects

--- script/index.js
+++ script/index.js
@@ -3,13 +3,27 @@ const todoInput = document.getElementById("todoInput")
 const todoList = document.getElementById("todoList")
 const todoInputHelp = document.getElementById("todoInputHelp")
 
-// Skapar själva todo-raden och lägger in texten användaren skrev.
+const todos = []
+
 const createTodo = (text) => {
+    return {
+        id: crypto.randomUUID(),
+        text: text,
+        done: false,
+        createdAt: new Date().toISOString()
+    }
+}
+
+const createTodoLi = (todo) => {
     const todoLiText = document.createElement("span")
-    todoLiText.textContent = text
+    todoLiText.textContent = todo.text
 
     const todoLi = document.createElement("li")
     todoLi.classList.add("list-group-item", "mt-3", "d-flex", "justify-content-between", "align-items-center")
+
+    if (todo.done) {
+        todoLi.classList.toggle("bg-success-subtle")
+    }
 
     todoLi.appendChild(todoLiText)
 
@@ -17,26 +31,29 @@ const createTodo = (text) => {
 }
 
 // Skapar en delete-knapp som tar bort just den todo-rad den hör till.
-const createButtonDelete = (todoLi) => {
+const createButtonDelete = (todo) => {
     const buttonDelete = document.createElement("button")
     buttonDelete.textContent = "x"
     buttonDelete.classList.add("btn", "btn-danger", "btn-sm")
 
     buttonDelete.addEventListener("click", () => {
-        todoLi.remove()
+        todos = todos.filter((currentTodo) => currentTodo.id !== todo.id)
+        renderApp()
     })
 
     return buttonDelete
 }
 
 // Skapar en knapp som markerar todo-raden som klar genom att växla klass.
-const createButtonDone = (todoLi) => {
+const createButtonDone = (todo) => {
     const buttonDone = document.createElement("button")
     buttonDone.textContent = "Done"
     buttonDone.classList.add("btn", "btn-success", "btn-sm")
 
     buttonDone.addEventListener("click", () => {
-        todoLi.classList.toggle("bg-success-subtle")
+        todo.done = !todo.done
+
+        renderApp()
     })
 
     return buttonDone
@@ -51,14 +68,12 @@ const createButtonWrapper = () => {
 }
 
 // Bygger ihop en komplett todo-rad med text, delete-knapp och done-knapp.
-const createLiElement = (text) => {
-    const todoLi = createTodo(text)
+const createLiElement = (todo) => {
+    const todoLi = createTodoLi(todo)
 
     const buttonWrapper = createButtonWrapper()
-
-    const buttonDelete = createButtonDelete(todoLi)
-
-    const buttonDone = createButtonDone(todoLi)
+    const buttonDelete = createButtonDelete(todo)
+    const buttonDone = createButtonDone(todo)
 
     buttonWrapper.appendChild(buttonDelete)
     buttonWrapper.appendChild(buttonDone)
@@ -67,25 +82,47 @@ const createLiElement = (text) => {
     return todoLi
 }
 
+const renderTodoListView = () => {
+    todoList.innerHTML = ""
+
+    todos.forEach((todo) => {
+        const todoElement = createLiElement(todo)
+
+        todoList.appendChild(todoElement)
+    })
+}
+
+const renderApp = () => {
+    renderTodoListView()
+}
+
 // När formuläret skickas skapas en ny todo som läggs till i listan.
 todoForm.addEventListener("submit", (event) => {
     event.preventDefault()
 
     const text = todoInput.value.trim()
 
-    const todoLi = createLiElement(text)
+    // const todoLi = createLiElement(text)
 
-    todoList.appendChild(todoLi)
+    const newTodo = createTodo(text)
+
+    todos.push(newTodo)
+
+    console.log(todos)
+
+    renderApp()
 
     todoInput.value = ""
     todoInput.focus()
 })
 
-/* 
-? event.preventDefault() 
+/*
+? event.preventDefault()
 
 Ett formulär försöker som standard skicka data och ladda om sidan.
 Eftersom vi vill hantera formuläret själva med JavaScript
 stoppar vi det vanliga beteendet med preventDefault().
 Annars hinner sidan laddas om innan vi kan jobba vidare med värdet.
 */
+
+
