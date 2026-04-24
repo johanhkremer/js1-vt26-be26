d7edd3fe5de9b305a9d68f9308114c2b25d2bc13
Johan Kremer <johan.h.kremer@gmail.com>
2026-04-24 11:55:24 +0200

    add forecast

--- script/index.js
+++ script/index.js
@@ -2,9 +2,10 @@ import { OPEN_WEATHER_API_KEY } from "./config.js"
 
 const cityWeatherForm = document.getElementById("cityWeatherForm")
 const cityWeatherInput = document.getElementById("cityWeatherInput")
-const weatherCard = document.getElementById("weatherCard")
+const weatherContainer = document.getElementById("weatherContainer")
+const forecastContainer = document.getElementById("forecastContainer")
 
-const getWeather = async (city) => {
+const getCurrentWeather = async (city) => {
     const [location] = await getCoordinates(city)
 
     if (!location) {
@@ -24,6 +25,18 @@ const getWeather = async (city) => {
     return data
 }
 
+const getForecast = async (city) => {
+    const [location] = await getCoordinates(city)
+
+    const { lat, lon } = location
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric&lang=sv`)
+
+    const data = await response.json()
+
+    return data
+}
+
 const getCoordinates = async (city) => {
     const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},SE&limit=1&appid=${OPEN_WEATHER_API_KEY}`)
 
@@ -36,40 +49,81 @@ const getCoordinates = async (city) => {
     return data
 }
 
-const renderWeatherCard = async (city) => {
+const renderCurrentWeatherCard = async (city) => {
     try {
-        const weatherCity = await getWeather(city)
-        console.log(weatherCity)
+        const weatherCity = await getCurrentWeather(city)
 
-        weatherCard.innerHTML = `
-    <h2>${weatherCity.name}</h2>
+        weatherContainer.innerHTML = `
+            <div class="weatherCard">
+                <h2>${weatherCity.name}</h2>
 
-    <img src="https://openweathermap.org/img/wn/${weatherCity.weather[0].icon}@2x.png"
-    alt="${weatherCity.weather[0].description}">
-    <p>Temperatur: ${weatherCity.main.temp}°C</p>
-    <p>Känns som: ${weatherCity.main.feels_like}°C</p>
-    <p>Väder: ${weatherCity.weather[0].description}</p>
-    <p>Luftfuktighet: ${weatherCity.main.humidity}%</p>
-    <p>Vind: ${weatherCity.wind.speed} m/s</p>
+                <img src="https://openweathermap.org/img/wn/${weatherCity.weather[0].icon}@2x.png"
+                alt="${weatherCity.weather[0].description}">
+                <p>Temperatur: ${weatherCity.main.temp}°C</p>
+                <p>Känns som: ${weatherCity.main.feels_like}°C</p>
+                <p>Väder: ${weatherCity.weather[0].description}</p>
+                <p>Luftfuktighet: ${weatherCity.main.humidity}%</p>
+                <p>Vind: ${weatherCity.wind.speed} m/s</p>
+            </div>
     `
-        weatherCard.classList.add("weatherCard")
+    } catch (error) {
+        console.log("Something went wrong: ", error)
+        weatherContainer.innerHTML = `
+        <p>Någontin gick fel: ${error.message}</p>
+        `
+        weatherContainer.classList.add("red")
+    }
+}
+
+const renderForcastCards = async (city) => {
+    try {
+        const forecastData = await getForecast(city)
+
+        const dailyForecasts = forecastData.list.filter((forecast) => {
+            return forecast.dt_txt.includes("12:00:00")
+        })
+
+        const forecastCards = dailyForecasts.map((forecastCard) => {
+            const date = forecastCard.dt_txt.split(" ")[0]
+
+            return `
+                <article class="forecastCard">
+                    <h3>${date}</h3>
+                    <img
+                        src="https://openweathermap.org/img/wn/${forecastCard.weather[0].icon}@2x.png"
+                        alt="${forecastCard.weather[0].description}"
+                    >
+                    <p>Temperatur: ${Math.round(forecastCard.main.temp)}°C</p>
+                    <p>Väder: ${forecastCard.weather[0].description}</p>
+                </article>
+            `
+        }).join("")
+
+        forecastContainer.innerHTML = forecastCards
 
     } catch (error) {
         console.log("Something went wrong: ", error)
-        weatherCard.innerHTML = `
+        forecastContainer.innerHTML = `
         <p>Någontin gick fel: ${error.message}</p>
         `
-        weatherCard.classList.add("red")
+        forecastContainer.classList.add("red")
     }
+}
+
+const renderApp = (city) => {
+    //Coordinates
+
+    renderCurrentWeatherCard(city)
+    renderForcastCards(city)
 }
 
 cityWeatherForm.addEventListener("submit", (event) => {
     event.preventDefault()
 
-    weatherCard.innerHTML = ""
-    weatherCard.classList.remove("red", "weatherCard")
+    weatherContainer.innerHTML = ""
+    weatherContainer.classList.remove("red", "weatherCard")
 
     const city = cityWeatherInput.value.trim()
 
-    renderWeatherCard(city)
+    renderApp(city)
 })
